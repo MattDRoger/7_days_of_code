e681c860c74cf0e1177776a758db5760a6be22cc
MattDRoger <mat_rgr@hotmail.com>
2022-07-14 16:52:26 -0300

    Modificações no visual e desenvolvendo o .js

--- Desafio do dia 7/desafio7.js
+++ Desafio do dia 7/desafio7.js
@@ -1,18 +1,23 @@
-let operacoes = document.querySelectorAll(".operacao");
-let sair = document.querySelector(".sair");
-let numeros = document.querySelectorAll("numero");
+const operacoes = document.querySelectorAll(".operacao");
+const sair = document.querySelector(".sair");
+const numeros = document.querySelectorAll("numero");
 const calculadora = document.querySelector(".calculadora");
+const inserir = document.querySelector(".inserir");
+const calcular = document.querySelector(".calcular");
+const valorInput = document.querySelector(".conteudo-resposta").value;
+const monitor = document.getElementById("monitor");
+
 let valorOperacao = "";
 
+
+
 function draw (){
-    operacaoSoma();
-    telaDigital();
-}
-
-
-function telaDigital (){
-    const 
-    
+    //operacaoSoma();
+    //operacaoSubtracao();
+    //operacaoMultiplicacao();
+    //operacaoDivisao();
+    //criaCalculo();
+    //numerosCalculadora();
 }
 
 function operacaoSoma (){
@@ -22,8 +27,52 @@ function operacaoSoma (){
 
    
     valorOperacao = "+";
-
-
     })
 }
 
+function operacaoSubtracao (){
+    operacoes[1].addEventListener("click", () => {
+        operacoes[1].classList.add("selecionado");
+        calculadora.classList.remove("hide");
+
+        
+        valorOperacao = "-";
+    })
+}
+
+function operacaoMultiplicacao (){
+    operacoes[2].addEventListener("click", () => {
+        operacoes[2].classList.add("selecionado");
+        calculadora.classList.remove("hide");
+
+   
+    valorOperacao = "*";
+    })
+}
+
+function operacaoDivisao (){
+    operacoes[3].addEventListener("click", () => {
+        operacoes[3].classList.add("selecionado");
+        calculadora.classList.remove("hide");
+
+        
+        valorOperacao = "/";
+    })
+}
+
+inserir.addEventListener("click", () => {
+    monitor.innerHTML = monitor +"<li>"+valorInput+"</li>";
+    
+    document.getElementById("monitor").innerHTML = monitor;
+})
+
+
+
+
+function numerosCalculadora (){
+    for (n = 0; n < numeros.length; n++)
+    
+    numeros[n].addEventListener("click", () => {
+        ;
+    })
+}
